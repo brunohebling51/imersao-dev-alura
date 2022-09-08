efb707a88be82ed098eb422109e85f0b7edb9894
Bruno Hebling <brunoalemao1@live.com>
2022-09-08 16:13:41 -0300

    second commit

---
+++ Dia 01 - Calculadora de Média/main.js
@@ -0,0 +1,14 @@
+let nome = 'Bruno';
+let notaDoPrimeiroBimestre = 9;
+let notaDoSegundoBimestre = 7;
+let notaDoTerceiroBimestre = 4;
+let notaDoQuartoBimestre = 2;
+let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+let notaFinalCerta = notaFinal.toFixed(1);
+
+
+console.log(`Bem vindo ${nome}`);
+console.log(notaFinalCerta);
+
+// Revisão
+// variaveis, string, console.log, toFixed, operacoes matematicas, concatenacao
